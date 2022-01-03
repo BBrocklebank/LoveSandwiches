16bbdaa289b836fc3c0e12659cbe95332518cfe0
Benjamin Brocklebank <benjamin.brocklebank@aol.com>
2022-01-03 17:00:26 +0000

    Feat: Added while loop to validation, requests will continue untill data passes validation checks

--- run.py
+++ run.py
@@ -21,14 +21,20 @@ def get_sales_data():
     """
     Get sales figures input from the user
     """
-    print('Please enter sales data from the last market.')
-    print('Data should be six numbers, separated by commas.')
-    print('Example: 10,20,30,40,50,60\n')
+    while True:
 
-    data_str = input('Enter your data here: ')
+        print('Please enter sales data from the last market.')
+        print('Data should be six numbers, separated by commas.')
+        print('Example: 10,20,30,40,50,60\n')
 
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        data_str = input('Enter your data here: ')
+
+        sales_data = data_str.split(",")
+        if validate_data(sales_data):
+            print('Data is Valid!')
+            break
+
+    return sales_data
 
 
 def validate_data(values):
@@ -37,7 +43,7 @@ def validate_data(values):
     Raises ValueError if strings cannot be converted into int,
     or if thee aren't exactly 6 values.
     """
-    print(values)
+
     try:
         [int(value) for value in values]
         if len(values) != 6:
@@ -46,6 +52,9 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f'Invalid data: {e}, please try again.\n')
+        return False
+       
+    return True
 
 
-get_sales_data()
+data = get_sales_data()
