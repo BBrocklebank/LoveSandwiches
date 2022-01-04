2a2646c3c592d9e9dbb4306c20a64cc8e1ceae7b
Benjamin Brocklebank <benjamin.brocklebank@aol.com>
2022-01-04 17:02:19 +0000

    Feat: Added function to get last five entries of sales

--- run.py
+++ run.py
@@ -91,6 +91,20 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
+def get_last_five_entries_sales():
+    """
+    Collects collumns of data from sales worksheet,
+    collectiing the last five entries and returning them as a list of lists.
+    """
+    sales = SHEET.worksheet('sales')
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    return columns
+
+
 def main():
     """
     Run all program functions
@@ -104,4 +118,5 @@ def main():
 
 
 print('Welcome to Love Sandwiches Data Automation\n')
-main()
+# main()
+get_last_five_entries_sales()
